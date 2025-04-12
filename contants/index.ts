14c963316bce3ec9b805c61452c611b7eeee0655
Phong Vo <phong.vo27ams@hcmut.edu.vn>
2025-04-12 23:07:18 +0700

    Initial commit; create Hero, Navbar, Footer component

---
+++ contants/index.ts
@@ -0,0 +1,57 @@
+export const manufacturers = [
+  "Chevrolet",
+  "Ford",
+  "Hyundai",
+  "Isuzu",
+  "Mazda",
+  "MG",
+  "Mitsubishi",
+  "Subaru",
+  "Toyota"
+]
+
+export const yearsOfProduction = [
+  { title: "Year", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" }
+]
+
+export const fuels = [
+  { title: "Fuel", value: "" },
+  { title: "Gas", value: "Gas" },
+  { title: "Electricity", value: "Electricity" },
+  { title: "Hybrid", value: "Hybrid" }
+]
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Business Relation", url: "/" },
+    ]
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a Friend", url: "/" },
+    ]
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "YouTube", url: "/" },
+      { title: "Facebook", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Instagram", url: "/" },
+    ]
+  }
+]
